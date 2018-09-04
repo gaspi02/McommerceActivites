4dcc649468542ded06be1a8524dfacb22dc8deb4
Gaspi02 <duringer.gaspard@gmail.com>
2018-09-04 16:55:40 +0200

    MAJ

--- microservice-expedition/src/main/java/com/mexpedition/model/Expedition.java
+++ microservice-expedition/src/main/java/com/mexpedition/model/Expedition.java
@@ -1,4 +1,61 @@
 package com.mexpedition.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Expedition {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(unique = true)
+    private Integer idCommande;
+
+    private int etat;
+
+    public Expedition() {
+    }
+
+    public Expedition(int id, Integer idCommande, int etat) {
+        this.id = id;
+        this.idCommande = idCommande;
+        this.etat = etat;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getIdCommande() {
+        return idCommande;
+    }
+
+    public void setIdCommande(Integer idCommande) {
+        this.idCommande = idCommande;
+    }
+
+    public int getEtat() {
+        return etat;
+    }
+
+    public void setEtat(int etat) {
+        this.etat = etat;
+    }
+
+    @Override
+    public String toString() {
+        return "Expedition{" +
+                "id=" + id +
+                ", idCommande=" + idCommande +
+                ", etat=" + etat +
+                '}';
+    }
 }
